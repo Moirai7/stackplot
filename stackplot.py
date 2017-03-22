21ddb94342bb6e80a345d3d42762a5e1d70c00fe
“Moirai7git config --global user.email “moirai.zhang@gmail.com <moirai.zhang@gmail.com>
2017-03-22 18:54:15 +0800

    update

--- stackplot.py
+++ stackplot.py
@@ -1,28 +1,49 @@
 import numpy as np
-import matplotlib
-matplotlib.use('PS')
-import  matplotlib.pyplot as plt
 
+def smooth(x,data):
+	from scipy.interpolate import spline
+	xs = np.linspace(0,len(x),len(x)*10)
+	new_data = []
+	for i in range(len(data)):
+		new_data.append(spline(x,data[i],xs))
+	return (xs,np.array(new_data))
 
-def showData(y):
-	x = xrange(50)
+def changeRow(data):
+	pass
+		
+
+def showData(x,y,baseline=0):
+	import  matplotlib.pyplot as plt
+	from matplotlib import colors
+	#plt.style.use('ggplot')
+	lens = len(y)-1
+	z = np.row_stack(range(lens+1))
+	normalize = colors.Normalize(vmax=z.max(),vmin=z.min()-5)
 	stack = np.cumsum(y, axis=0, dtype=np.promote_types(y.dtype, np.float32))
 
 	fig = plt.figure()
 	axes = fig.add_subplot(111)
-	axes.fill_between(x, 0, stack[0, :],facecolor="#CC6666", alpha=.7) 
-	for i in xrange(len(y)-1):
-		label = 1.0/(1.0+exp(-i))
-		axes.fill_between(x, stack[i,:], stack[i+1, :],facecolor=plt.cm.RdYlBu(label), alpha=.7) 
+	cmap=plt.get_cmap('Blues')
+	axes.fill_between(x, baseline, stack[0, :],color=cmap(normalize(z[lens])),where=stack[0, :]>baseline ,alpha=1,edgecolor='k') 
+	for i in range(1,len(y)):
+		color = cmap(normalize(z[lens-i]))
+		axes.fill_between(x, stack[i-1,:], stack[i, :],color=color,edgecolor='k',where=stack[0, :]>baseline) 
 	plt.show()
 
 def procData():
 	import json
-	with open('data.json') as data_file:    
+	with open('data.json') as data_file:	
 		data = json.load(data_file)
 	data = np.row_stack(data)
-	print len(data)
-	showData(data)
+	x = range(len(data[0]))
+	print 'pic without smoothing...'
+	showData(x,data)
+	print 'pic with smooth...'
+	x,data = smooth(x,data)
+	showData(x,data)
+	print 'pic with novel design...'
+	data = changeRow(data)
+	showData(x,data)
 	pass
 
 if __name__ == '__main__':
